dc2398dc823517c1b50330fca3dace88d1a25b09
Diana Dai <dianadai@DianatekiMacBook-Air.local>
2021-11-15 09:16:39 +1100

    update

--- assets/swatch.js
+++ assets/swatch.js
@@ -1,5 +1,10 @@
 $('.Color-label span').each(function () {
   if($(this).attr('class').indexOf('swatch_color') > -1){
-    $(this).attr('style', '');
+    var styleValue = $(this)[0].style.color;
+    var cssValue = $(this).css('color');
+    var parentcssValue = $(this).parent().css('color');
+    if (cssValue === parentcssValue) {
+      $(this).css('color', $(this).attr('data-color'));
+    }
   }
 })
